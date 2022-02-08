cde9f43c108db3940ccee932f246ed4e5a6d9fd8
Brian Graziano <72954106+afathom@users.noreply.github.com>
2022-02-08 03:46:08 +0000

    Added loop

--- main.cs
+++ main.cs
@@ -3,13 +3,13 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
-  
-  Console.WriteLine("Enter a number: ");
+  while (true)
+  {Console.WriteLine("Enter a number: ");
   Int32 myNumber = Convert.ToInt32(Console.ReadLine());
 
   if (myNumber % 2 == 0) Console.WriteLine("Number is even.");
   else Console.WriteLine("Number is odd.");
-
+  }
     
   }
 }
