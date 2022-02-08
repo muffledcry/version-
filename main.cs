90858977ebe1273226498f5be94dd90fbda96a9a
Brian Graziano <>
2022-02-08 03:08:10 +0000

    We added odd or even.

--- main.cs
+++ main.cs
@@ -3,5 +3,11 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+  
+  Console.WriteLine("Enter a number: ");
+  Int32 myNumber = Convert.ToInt32(Console.ReadLine());
+
+  if (myNumber % 2 == 0) Console.WriteLine("Number is even.");
+  else Console.WriteLine("Number is odd.");
   }
 }
